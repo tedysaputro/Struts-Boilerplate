71d00f70fa8c9c59f02e6fb5e76e8f0568667611
Tedy Tri Saputro <tedy.saputro@ruangtedy.net>
2017-05-22 05:37:54 +0700

    add hibernate

--- src/main/java/com/embeddednesia/struts/model/User.java
+++ src/main/java/com/embeddednesia/struts/model/User.java
@@ -1,10 +1,26 @@
 package com.embeddednesia.struts.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.Table;
+import javax.persistence.InheritanceType;
+
+@Entity()
+@Table(name="user")
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
 public class User {
 
+	@Id
+	@Column(name ="ID", length = 100, nullable = false)
 	private int id;
+	@Column(name="name")
 	private String name;
+	@Column(name="user_name")
 	private String userName;
+	
+	@Column(name="password")
 	private String password;
 	
 	public User() {
